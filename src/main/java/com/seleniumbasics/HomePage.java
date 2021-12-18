8d31bfe17037c2bf71b5164904ce267398cef8a0
MSDOMAIN1\ajadha3 <amruta.jadhav@morningstar.com>
2021-12-18 20:27:23 +0530

    home page feature added

---
+++ src/main/java/com/seleniumbasics/HomePage.java
@@ -0,0 +1,17 @@
+package com.seleniumbasics;
+
+public class HomePage {
+
+	public HomePage() {
+		System.out.println("home page class");
+	}
+
+	public void logoHome() {
+		System.out.println("home page url");
+		System.out.println("logo added action");
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
