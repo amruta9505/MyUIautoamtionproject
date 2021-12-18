215bbc0e03b84acb3869ca72d13921611d65aead
MSDOMAIN1\ajadha3 <amruta.jadhav@morningstar.com>
2021-12-18 18:57:48 +0530

    added registration page

---
+++ src/main/java/com/seleniumbasics/RegistrationPage.java
@@ -0,0 +1,16 @@
+package com.seleniumbasics;
+
+public class RegistrationPage {
+	public RegistrationPage() {
+		System.out.println("registration page class");
+	}
+
+	public void signup() {
+		System.out.println("signup page");
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
